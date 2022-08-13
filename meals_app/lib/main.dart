c81cb3fd87cafdc11bf01850fa866c4d0b298c4c
Lizzz2020 <lizagildisch@gmail.com>
2022-08-12 21:07:54 -0700

    unknown route

--- meals_app/lib/main.dart
+++ meals_app/lib/main.dart
@@ -12,35 +12,47 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'DeliMeals',
-        theme: ThemeData(
-            primarySwatch: Colors.pink,
-            accentColor: Colors.amber,
-            canvasColor: Color.fromRGBO(225, 254, 229, 1),
-            fontFamily: 'Raleway',
-            textTheme: ThemeData.light().textTheme.copyWith(
-                  body1: TextStyle(
-                    color: Color.fromRGBO(20, 51, 51, 1),
-                  ),
-                  body2: TextStyle(
-                    color: Color.fromRGBO(20, 51, 51, 1),
-                  ),
-                  title: TextStyle(
-                    fontSize: 20,
-                    fontFamily: "RobotoCondensed",
-                    fontWeight: FontWeight.bold,
-                  ),
-                )),
-        initialRoute: '/',
-        //home: CategoriesScreen(),
+      title: 'DeliMeals',
+      theme: ThemeData(
+          primarySwatch: Colors.pink,
+          accentColor: Colors.amber,
+          canvasColor: Color.fromRGBO(225, 254, 229, 1),
+          fontFamily: 'Raleway',
+          textTheme: ThemeData.light().textTheme.copyWith(
+                body1: TextStyle(
+                  color: Color.fromRGBO(20, 51, 51, 1),
+                ),
+                body2: TextStyle(
+                  color: Color.fromRGBO(20, 51, 51, 1),
+                ),
+                title: TextStyle(
+                  fontSize: 20,
+                  fontFamily: "RobotoCondensed",
+                  fontWeight: FontWeight.bold,
+                ),
+              )),
+      initialRoute: '/',
+      //home: CategoriesScreen(),
 
-        routes: {
-          '/': (ctx) => CategoriesScreen(),
+      routes: {
+        '/': (ctx) => CategoriesScreen(),
 
-          CategoryMealsScreen.routeName: (ctx) =>
-              CategoryMealsScreen(), //or you can use the link style - "'/category_meals'"
-          mealDetailScreen.routeName: (ctx) => mealDetailScreen(),
-        });
+        CategoryMealsScreen.routeName: (ctx) =>
+            CategoryMealsScreen(), //or you can use the link style - "'/category_meals'"
+        mealDetailScreen.routeName: (ctx) => mealDetailScreen(),
+      },
+      onGenerateRoute: (settings) {
+        //navigates user to specified route in case the route the user wanted to go to doesnt exist by name
+        print(settings.arguments);
+        return MaterialPageRoute(
+          builder: (ctx) => CategoriesScreen(),
+        );
+      },
+      onUnknownRoute: (settings) {
+        //when everything above fails, it send back to original page (instead of crashing)
+        return MaterialPageRoute(builder: (ctx) => CategoriesScreen());
+      },
+    );
   }
 }
 
--- meals_app/lib/screens/meal_detail_screen.dart
+++ meals_app/lib/screens/meal_detail_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../dummy_data.dart';
 
 class mealDetailScreen extends StatelessWidget {
   static const routeName = '/meal-detail';
@@ -6,10 +7,25 @@ class mealDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final mealId = ModalRoute.of(context).settings.arguments as String;
-
+    final selectedMeal = DUMMY_MEALS.firstWhere((meal) => meal.id == mealId);
     return Scaffold(
-      appBar: AppBar(title: Text(mealId)),
-      body: Center(child: Text('the meal - $mealId')),
+      appBar: AppBar(title: Text('${selectedMeal.title}')),
+      body: Column(
+        children: <Widget>[
+          Container(
+            height: 300,
+            width: double.infinity,
+            child: Image.network(selectedMeal.imageUrl, fit: BoxFit.cover),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 10),
+            child:
+                Text('Ingredients', style: Theme.of(context).textTheme.title),
+          ),
+          ListView()
+          //dont use ListView with Columns because both try to take as much space as possible which will throw an error, instead wrap in container first
+        ],
+      ),
     );
   }
 }
