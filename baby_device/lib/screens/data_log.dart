4e13bd38303ce310b00ad91d7049e2f918175846
Lizzz2020 <lizagildisch@gmail.com>
2022-08-23 00:34:22 -0700

    baby_device start project

---
+++ baby_device/lib/screens/data_log.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class DataLog extends StatefulWidget {
+  @override
+  State<DataLog> createState() => _DataLogState();
+}
+
+class _DataLogState extends State<DataLog> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
